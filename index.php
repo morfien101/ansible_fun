fe51bc62118ff49f71e189103b14c7b873daf803
Randy Coburn <randy@ecs-digital.co.uk>
2017-06-29 11:52:57 +0100

    fix some syntax errors

--- index.php
+++ index.php
@@ -4,10 +4,10 @@
 </head>
 <body>
 <?php
-include creds.php;
+include 'creds.php';
 
 // Create connection
-$conn = new mysqli($servername, $username, $password, $dbname);
+$conn = new mysqli(servername, username, password, dbname);
 // Check connection
 if ($conn->connect_error) {
     die("Connection failed: " . $conn->connect_error);
